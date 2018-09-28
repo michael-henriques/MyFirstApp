208062aeb087f467be219e3edbcf0d90d634104a
Michael <bonomozz@hotmail.com>
2018-09-28 08:22:59 -0400

    This could work!

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo cd /pipeline/source/app/MyFirstAspNetCoreApp/publish/
-sudo dotnet MyFirstAspNetCoreApp.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
+cd /pipeline/source/app/MyFirstAspNetCoreApp/publish/
+dotnet MyFirstAspNetCoreApp.dll --server.urls=http://0.0.0.0:${PORT-"8080"}

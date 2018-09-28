d32fb740c718536dd43571b7b68f27aaecb1c689
Michael <bonomozz@hotmail.com>
2018-09-27 22:41:11 -0400

    Trying the shell again

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,2 +1,2 @@
-sudo cd /pipeline/source/app/publish
-sudo dotnet MyFirstAspNetCoreApp.dll
+cd /pipeline/source/app/publish
+dotnet MyFirstAspNetCoreApp.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
